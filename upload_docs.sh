ceb8e92d0df3abe80af008db6253dab8eb85a067
Joe Delfino <jdelfino@brandeis.edu>
2025-04-13 20:21:57 +0000

    upload docs script final

--- upload_docs.sh
+++ upload_docs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-jar -xvf lib/target/atg-api-1.4.1-SNAPSHOT-javadoc.jar -C docs
 # Need to get a connection string from the storage account in azure portal to make this work
+cd lib
+mvn clean package
+cd ..
+rm -r docs
+jar -xvf lib/target/atg-api-*-SNAPSHOT-javadoc.jar -C docs
+
 az storage blob upload-batch   --account-name atgapi --destination-path="v1"   --destination=\$web   --source docs --overwrite
